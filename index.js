faab34ee4ec19daea4f546fefc709fb7b1d5c838
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-25 18:13:11 +0600

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,44 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD)
+//mongodb
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ycofkd3.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+//function
+// async function run(){
+
+//     try{
+//        const mobileCategoryCollection = client.db('mobileCategory').collection('');
+
+//        app.get('/categories', async(req,res)=>{
+//         const query = {};
+//         const categories = await mobileCategoryCollection.find(query).toArray();
+//         res.send(categories);
+//        })
+//     }
+//     finally{
+
+//     }
+// }
+
+// run().catch(console.log);
+
 
 
 app.get('/',(req,res)=>{
